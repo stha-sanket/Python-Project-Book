f68f9b1c75c2b5728a3eef9e3fbe26c39dfd373b
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-08-02 22:03:20 +0545

    Update alu-cross.py

---
+++ tic-tac-toe/alu-cross.py
@@ -0,0 +1,11 @@
+# Lets make a board first
+''''
+should be like this
+  |   |  
+----------
+  |   |  
+----------
+  |   |  
+----------
+i.e this "-" should be 10 of them to divide them and "|" should be in between rows
+''''
