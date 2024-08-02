860cfc8ec11ebc6fffdf63c6072f55184bc5177a
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-08-02 22:03:40 +0545

    Update alu-cross.py

--- tic-tac-toe/alu-cross.py
+++ tic-tac-toe/alu-cross.py
@@ -9,3 +9,7 @@ should be like this
 ----------
 i.e this "-" should be 10 of them to divide them and "|" should be in between rows
 ''''
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 10)
