d2627208b0e0272cfe2264d25122ac869385cf3e
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-08-02 22:12:03 +0545

    Update alu-cross.py

--- tic-tac-toe/alu-cross.py
+++ tic-tac-toe/alu-cross.py
@@ -19,6 +19,10 @@ defining how one can win?
 so it basic 3 "X" or "O" in horizontal/vertical or diagonal right, that means toomuch of matrix
 lets say "X" or "O" is know as mark and our board is designed in matrix form i.e [0,1,2]
 then,
+
 possibe win chance;
-- {0}{0},{1}{1},{2}{2},
+- {0}{0},{1}{1},{2}{2}
+- {0}{2},{1,1},{2}{0}
+- {0}{colum},{0}{colum},{0}{colum},
+- if row[0] == mark and row[1] == mark and row[2] == mark:
 ''''
