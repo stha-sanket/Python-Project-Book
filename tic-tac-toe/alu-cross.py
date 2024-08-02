8ab2cf064167739f3cc19b8204b22b08d2c4f065
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-08-02 22:08:29 +0545

    Update alu-cross.py

--- tic-tac-toe/alu-cross.py
+++ tic-tac-toe/alu-cross.py
@@ -13,3 +13,12 @@ def print_board(board):
     for row in board:
         print(" | ".join(row))
         print("-" * 10)
+
+''''
+defining how one can win?
+so it basic 3 "X" or "O" in horizontal/vertical or diagonal right, that means toomuch of matrix
+lets say "X" or "O" is know as mark and our board is designed in matrix form i.e [0,1,2]
+then,
+possibe win chance;
+- {0}{0},{1}{1},{2}{2},
+''''
