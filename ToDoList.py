5070b652c1d91f32b72e66c97af309d5531aac27
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-08-03 22:44:54 +0545

    Update ToDoList.py

--- ToDoList.py
+++ ToDoList.py
@@ -1,2 +1,9 @@
 # this is how we make a to-do list in python well quite a small project but still a quite a good start
-todo_file = "todo_list.txt"
+todo_file = "todo_list.txt" # this is the file we will be working on
+def load_tasks():
+    try:
+        with open(todo_file, "r") as f:
+            tasks = f.readlines()
+        return [task.strip() for task in tasks]
+    except FileNotFoundError:
+        return []
