6f6d1cffed936a0507569cd54fd4d1130b542456
sanket shrestha <133840007+stha-sanket@users.noreply.github.com>
2024-08-04 23:04:56 +0545

    Update ToDoList.py

--- ToDoList.py
+++ ToDoList.py
@@ -1,5 +1,7 @@
 # this is how we make a to-do list in python well quite a small project but still a quite a good start
 todo_file = "todo_list.txt" # this is the file we will be working on
+
+# Function to load tasks from the to-do list file
 def load_tasks():
     try:
         with open(todo_file, "r") as f:
